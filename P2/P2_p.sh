da1fcc56e35643e39ef2904b630d46e8308b0e82
Nicofero <nicofero01@gmail.com>
2022-05-11 16:44:51 +0200

    Blocking

--- P2/P2_p.sh
+++ P2/P2_p.sh
@@ -1,9 +1,16 @@
 gcc -o II -O0 p2_apartado2.c
-gcc -o III -mavx2 p2_apartado3.c
+    gcc -O0 p2_apartado2_block.c -o IIb
 
-echo -e "II"
-./II 250 ex.r>a.txt
-echo -e "\n---------------------------------------\nIII"
-./III 250 ex.r>t.txt
+echo -n "NC=c(">>pr.r
+for i in {0..100}
+do
+    ./II 250 pr.r>>log1.log
+done
+echo ")">>pr.r
 
-diff a.txt t.txt
+echo -n "NC=c(">>pr.r
+for i in {0..100}
+do
+    ./IIb 250 pr.r>>log1.log
+done
+echo ")">>pr.r
--- P2/p2_apartado2_block.c
+++ P2/p2_apartado2_block.c
@@ -50,16 +50,34 @@ double get_counter();
  return result;
  }
 
+ int compare (const void * a,const void * b){
+  return ( *(double*)a - *(double*)b );
+}
+
+void free_2p(double **A,int fil,int col){
+    int i,j;
+    for(i=0;i<fil;i++){
+        free(A[i]);
+    }
+    free(A);
+}
+
 int main(int argc, char *argv[]){
 
-    double **a, **b, *c,**d,*e,f,ck; // Matrices y vector de entrada que almacenan valores aleatorios
-    int *ind, i, j, k,ii,jj,r;          // Vector desordenado aleatoriamente que contiene índices de fila/columna sin que se repitan
+    double **a, **b, *c,**d,*e,f,ck[10]; // Matrices y vector de entrada que almacenan valores aleatorios
+    int *ind, i, j,k, l,ii,jj;          // Vector desordenado aleatoriamente que contiene índices de fila/columna sin que se repitan
     int N;                      //Tamaño de la matriz
+    FILE *p;
 
-    if( argc != 2 ){
+    if( argc != 3 ){
       printf("Número de argumentos incorrecto\n");
       exit(EXIT_FAILURE);
     }
+
+    if ((p=fopen(argv[2],"a+"))==NULL){
+        perror("ERROR: ");
+        exit (-1);
+    }    
 
     N = atoi(argv[1]);
 
@@ -106,28 +124,52 @@ int main(int argc, char *argv[]){
         ind[i]=ind[k];
         ind[k] = j;
     }
-    start_counter();
+    for(l=0;l<10;l++){
+        start_counter();
 
-    // Inicialización de d
-    /*
-    for (i = 0; i < N; i++){ //filas
-        for (j = 0; j < N; j++){ // columnas
-            d[i*N + j] = 0;
+    
+        for (i=0; i< N; i+=block_size) {
+            for (j=0; j< N; j+=block_size) {
+                for (ii=i; ii<i+block_size; ii++) {
+                    for (jj=j; jj<j+block_size; jj++) {
+                        d[ii][jj]=0;
+                        d[ii][jj] += 2*a[ii][0]*(b[0][jj]-c[0]);
+                        d[ii][jj] += 2*a[ii][(1)]*(b[(1)][jj]-c[(1)]);
+                        d[ii][jj] += 2*a[ii][(2)]*(b[(2)][jj]-c[(2)]);
+                        d[ii][jj] += 2*a[ii][(3)]*(b[(3)][jj]-c[(3)]);
+                        d[ii][jj] += 2*a[ii][(4)]*(b[(4)][jj]-c[(4)]);
+                        d[ii][jj] += 2*a[ii][(5)]*(b[(5)][jj]-c[(5)]);
+                        d[ii][jj] += 2*a[ii][(6)]*(b[(6)][jj]-c[(6)]);
+                        d[ii][jj] += 2*a[ii][(7)]*(b[(7)][jj]-c[(7)]);
+                    }
+                }
+            }
         }
-    }*/
 
-    for (i = 0,f=0; i < N; i+=5){
-        e[i] = d[ind[i]][ind[i]] / 2;
-        e[i+1] = d[ind[i+1]][ind[i+1]] / 2;
-        e[i+2] = d[ind[i+2]][ind[i+2]] / 2;
-        e[i+3] = d[ind[i+3]][ind[i+3]] / 2;
-        e[i+4] = d[ind[i+4]][ind[i+4]] / 2;
-        f += e[i] + e[i+1] + e[i+2] + e[i+3] + e[i+4];
+        for (i = 0,f=0; i < N; i+=5){
+            e[i] = d[ind[i]][ind[i]] / 2;
+            e[i+1] = d[ind[i+1]][ind[i+1]] / 2;
+            e[i+2] = d[ind[i+2]][ind[i+2]] / 2;
+            e[i+3] = d[ind[i+3]][ind[i+3]] / 2;
+            e[i+4] = d[ind[i+4]][ind[i+4]] / 2;
+            f += e[i] + e[i+1] + e[i+2] + e[i+3] + e[i+4];
+        }
+
+        ck[l]=get_counter();
     }
 
+    qsort(ck,10,sizeof(double),compare);    //Ordenamos el vector de tiempos
+
     printf("f=%lf\n", f);
+    printf("\nClocks=%1.10lf \n",((ck[4]+ck[5])/2));    //Imprimimos la mediana de los tiempos
 
-    ck=get_counter();
+    if(N!=3000)   fprintf(p,"%lf,",((ck[4]+ck[5])/2));  //Impresion archivo .r
+    else    fprintf(p,"%lf)\n",((ck[4]+ck[5])/2));
 
-    printf("\nClocks=%1.10lf \n",ck);
+    //Liberacion de memoria
+    free_2p(a,N,8);
+    free_2p(b,8,N);
+    free(c);
+    free_2p(d,N,N);
+    free(e);
 }
